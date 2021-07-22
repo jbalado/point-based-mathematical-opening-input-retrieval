84593a78170c7ccceebeaef90c5367de2b41732c
Jesús Balado Frías <43201161+jbalado@users.noreply.github.com>
2021-07-22 18:48:17 +0200

    Add files via upload

--- Example.m
+++ Example.m
@@ -18,3 +18,21 @@ data_dilated = pc_mo_dilate(data_eroded,data_discarted,SE);
 
 % Export to HDD
 dlmwrite("cubo3d_opened_Z.txt",data_dilated,'delimiter',' ','precision',10);
+
+
+%% Example for a horizontal (X-oriented) opeining
+
+% SE definition (first point is the Reference point)
+SE = [0 0 0; -0.3 0 0; 0.3 0 0];
+
+% Erosion
+[data_eroded,data_discarted] = pc_mo_erode(data,SE);
+
+dlmwrite("data_eroded.txt",data_eroded,'delimiter',' ','precision',10);
+dlmwrite("data_discarted.txt",data_discarted,'delimiter',' ','precision',10);
+
+% Dilation
+data_dilated = pc_mo_dilate(data_eroded,data_discarted,SE);
+
+% Export to HDD
+dlmwrite("cubo3d_opened_X.txt",data_dilated,'delimiter',' ','precision',10);
